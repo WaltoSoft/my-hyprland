6700f681382290f5a9e4a9bc5cccbb7f7ca88bc3
Billy Walton <billyjwalton@outlook.com>
2024-12-01 11:23:08 -0600

    Fixed typo

--- install/includes/header.sh
+++ install/includes/header.sh
@@ -1,6 +1,6 @@
 # Some colors
 clear
-_displayFigma "Hyprland Install"
+_displayFiglet "Hyprland Install"
 echo "Version: ${MY_HYPRLAND_VERSION}"
 echo "Billy Walton's Personal Hyprland Configuration"
 echo "Use at your own risk!"
