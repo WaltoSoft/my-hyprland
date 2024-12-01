2a5368e9aca75a961070e46f54c22c3ccd8dda9e
Billy Walton <165430091+WaltoSoft@users.noreply.github.com>
2024-12-01 14:39:47 -0600

    Update preparation.sh
    Fixed Double quote that didn't have a matching close double quote

--- install/includes/preparation.sh
+++ install/includes/preparation.sh
@@ -5,4 +5,4 @@ if [ -d $HYPRLAND_FOLDER/$MY_HYPRLAND_VERSION ] ;then
     rm -rf $HYPRLAND_FOLDER/$MY_HYPRLAND_VERSION
 fi
 cp -r dotfiles $HYPRLAND_FOLDER/$MY_HYPRLAND_VERSION
-echo ":: my-hyprland configuration prepared in ${HYPRLAND_FOLDER}/${MY_HYPRLAND_VERSION}
+echo ":: my-hyprland configuration prepared in ${HYPRLAND_FOLDER}/${MY_HYPRLAND_VERSION}"
